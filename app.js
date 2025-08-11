e38ac9a3b1bfb8b04e157d8c56dc5e8dbe851f41
mohamed achraf <achrafelbey07@gmail.com>
2025-08-11 12:53:40 +0100

    add live reload

--- app.js
+++ app.js
@@ -1,97 +1,113 @@
-const express = require("express");
-const app = express();
+  const express = require("express");
+  const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-const mongoose = require('mongoose');
-const MyData = require("./modelss/dataShema");
-app.set('view engine','ejs');
-const users = [];
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  const mongoose = require('mongoose');
+  const MyData = require("./modelss/dataShema");
+  app.set('view engine','ejs');
+  app.use(express.static('public'));
 
-// HTTP METHODS
-// GET - Retrive Data
-app.get("/", (request, response) => {
-  response.send("Welcome to home!");
-});
+  const livereload = require("livereload");
+  const connectLivereload = require("connect-livereload");
+  app.use(connectLivereload());
 
+  const liveReloadServer = livereload.createServer();
+  liveReloadServer.watch(__dirname + "/views"); // راقب مجلد الـ EJS
+  liveReloadServer.watch(__dirname + "/public"); // لو عندك CSS/JS
 
-//res home page 
-app.get("/home", (req, res) => {
-  MyData.find().then((result) => {
-    console.log(result)
-    res.render("home" , {mytitle: "home page" , name : result.length > 0 ? result[0].username : "no user" , arr : result } )
-  }).catch((err) => {
-    console.log(err)
-  })
-});
-
-app.get("/users", (request, response) => {
-  if (users.length == 0) {
-    response.status(404).send("No users found!");
-    return;
-  }
-  response.status(200).send(users);
-});
-
-// POST - Create data
-app.post("/users", (request, response) => {
-  console.log(request.body);
-  const user = request.body;
-  const findUser = users.find((x) => x.id === user.id);
-  if (findUser) {
-    response.status(400).send("User already exists");
-    return;
-  }
-  users.push(user);
-  response.status(201).send("Created!");
-});
-
-
-// DELETE - Remove data
-app.delete('/users/:id', (request, response) => {
-  const { id } = request.params
-  const findUserIndex = users.findIndex((x) => x.id === id)
-  if (findUserIndex == -1) {
-    response.status(400).send("User not found!")
-    return
-  }
-  users.splice(findUserIndex, 1)
-  response.status(200).send("User deleted successfully!")
-})
-
-// PUT - Update user (partial update allowed)
-app.put("/users/:id", (req, res) => {
-  const { id } = req.params;
-  const updatedData = req.body;
-
-  const index = users.findIndex((u) => u.id === id);
-  if (index === -1) {
-    return res.status(404).send("User not found!");
-  }
-
-  users[index] = { ...users[index], ...updatedData };
-  res.status(200).send("User updated successfully!");
-});
-
-//DB 
-mongoose.connect('mongodb+srv://achrafelbey07:kziD5aHx6YvEfwGF@cluster0.sgmbtel.mongodb.net/all-data?retryWrites=true&w=majority&appName=Cluster0')
-  .then(() => {
-
-    app.listen(3000, () => {
-      console.log("Started on port 3000");
-    });
-
-  })
-  .catch((err) => { console.log(err) });
-
-///////////
-
-app.post("/home", (req, res) => {
-  console.log(req.body);
-  const myData = new MyData(req.body);
-  myData.save().then(() => {
-    res.redirect("/home");
-  }).catch((err) => {
-    console.log(err);
+  liveReloadServer.server.once("connection", () => {
+    setTimeout(() => {
+      liveReloadServer.refresh("/");
+    }, 100);
   });
-});
+
+  const users = [];
+
+  // HTTP METHODS
+  // GET - Retrive Data
+  app.get("/", (request, response) => {
+    response.send("Welcome to home!");
+  });
+
+
+  //res home page 
+  app.get("/home", (req, res) => {
+    MyData.find().then((result) => {
+      console.log(result)
+      res.render("home" , {mytitle: "home page" , name : result.length > 0 ? result[0].username : "no user" , arr : result } )
+    }).catch((err) => {
+      console.log(err)
+    })
+  });
+
+  app.get("/users", (request, response) => {
+    if (users.length == 0) {
+      response.status(404).send("No users found!");
+      return;
+    }
+    response.status(200).send(users);
+  });
+
+  // POST - Create data
+  app.post("/users", (request, response) => {
+    console.log(request.body);
+    const user = request.body;
+    const findUser = users.find((x) => x.id === user.id);
+    if (findUser) {
+      response.status(400).send("User already exists");
+      return;
+    }
+    users.push(user);
+    response.status(201).send("Created!");
+  });
+
+
+  // DELETE - Remove data
+  app.delete('/users/:id', (request, response) => {
+    const { id } = request.params
+    const findUserIndex = users.findIndex((x) => x.id === id)
+    if (findUserIndex == -1) {
+      response.status(400).send("User not found!")
+      return
+    }
+    users.splice(findUserIndex, 1)
+    response.status(200).send("User deleted successfully!")
+  })
+
+  // PUT - Update user (partial update allowed)
+  app.put("/users/:id", (req, res) => {
+    const { id } = req.params;
+    const updatedData = req.body;
+
+    const index = users.findIndex((u) => u.id === id);
+    if (index === -1) {
+      return res.status(404).send("User not found!");
+    }
+
+    users[index] = { ...users[index], ...updatedData };
+    res.status(200).send("User updated successfully!");
+  });
+
+  //DB 
+  mongoose.connect('mongodb+srv://achrafelbey07:kziD5aHx6YvEfwGF@cluster0.sgmbtel.mongodb.net/all-data?retryWrites=true&w=majority&appName=Cluster0')
+    .then(() => {
+
+      app.listen(3000, () => {
+        console.log("Started on port 3000");
+      });
+
+    })
+    .catch((err) => { console.log(err) });
+
+  ///////////
+
+  app.post("/home", (req, res) => {
+    console.log(req.body);
+    const myData = new MyData(req.body);
+    myData.save().then(() => {
+      res.redirect("/home");
+    }).catch((err) => {
+      console.log(err);
+    });
+  });
